bc28a347c9c2aaa09fbc13218ff0b649a6deddb8
Soviet.Dog <soviet.dog1945@proton.me>
2023-03-06 11:39:14 +0200

    Task 1 is done

---
+++ Task1.py
@@ -0,0 +1,7 @@
+print("Мультіпарадигмальні мови програмування, Task 1")
+print("Ванда Никита Денисович")
+teacher_name = ['Владислав']
+for i in range(0, 44):
+    teacher_name.append('Владислав')
+print(', '.join(teacher_name))
+print(123.8 + (11 - 21.1/2)/(87 - 32.2))
