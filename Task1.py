890707eeadc17108a34d3efb7ac6739d8e052705
Soviet.Dog <soviet.dog1945@proton.me>
2023-03-07 17:39:31 +0200

    correct list

--- Task1.py
+++ Task1.py
@@ -1,7 +1,5 @@
 print("Multiparadigm programming languages, Task 1")
 print("Vanda Nikita Denisovich")
-teacher_name = []
-for i in range(45):
-    teacher_name.append('Vladislav')
+teacher_name = ['Vladislav' for i in range(45)]
 print(', '.join(teacher_name))
 print(123.8 + (11 - 21.1 / 2) / (87 - 32.2))
