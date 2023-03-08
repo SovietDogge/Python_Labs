1b45bf8e357776a0bd2438cc3ae7f4a4c27d404a
Soviet.Dog <soviet.dog1945@proton.me>
2023-03-08 12:25:11 +0200

    add try except blocks

--- Task_3.py
+++ Task_3.py
@@ -2,19 +2,24 @@
 # Vanda Nikita Denisovich
 import math
 
-while True:
-    x = float(input())
-    if x == 1:
-        print('Incorrect value')
-    else:
-        break
+try:
 
-while True:
-    a = float(input())
-    if a == -2:
-        print('Incorrect value')
-    else:
-        break
+    while True:
+        x = float(input())
+        if x == 1:
+            print('Incorrect value')
+        else:
+            break
 
-y = (x ** 3 + 2 * a * x + 3) / ((x - 1) ** 2) + (math.cos(x ** 2)) / (a + 2)
-print(y)
+    while True:
+        a = float(input())
+        if a == -2:
+            print('Incorrect value')
+        else:
+            break
+
+    y = (x ** 3 + 2 * a * x + 3) / ((x - 1) ** 2) + (math.cos(x ** 2)) / (a + 2)
+    print(y)
+
+except ValueError:
+    print('Incorrect input')
