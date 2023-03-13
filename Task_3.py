a62a9f6cf1c6f7900356e25e43750e37d9da4919
Soviet.Dog <soviet.dog1945@proton.me>
2023-03-13 08:39:20 +0200

    add some fixes

--- Task_3.py
+++ Task_3.py
@@ -1,25 +1,19 @@
 # Multiparadigm programming languages, Task 1
 # Vanda Nikita Denisovich
 import math
+TEMPLATE = 'Incorrect value'
 
 try:
 
     while True:
         x = float(input())
-        if x == 1:
-            print('Incorrect value')
-        else:
-            break
-
-    while True:
         a = float(input())
-        if a == -2:
-            print('Incorrect value')
+        if x == 1 or a == -2:
+            print(TEMPLATE)
         else:
             break
+except ValueError:
+    print('Incorrect input')
 
     y = (x ** 3 + 2 * a * x + 3) / ((x - 1) ** 2) + (math.cos(x ** 2)) / (a + 2)
     print(y)
-
-except ValueError:
-    print('Incorrect input')
