274698549cd73686a1b3866f5a69fa5ba466399f
Soviet.Dog <soviet.dog1945@proton.me>
2023-03-13 08:45:59 +0200

    add try except

--- Task_2.py
+++ Task_2.py
@@ -1,9 +1,18 @@
 # Multiparadigm programming languages, Task 1
 # Vanda Nikita Denisovich
 
-x = float(input())
-y = float(input())
-z = float(input())
+try:
+
+    while True:
+        x = float(input())
+        y = float(input())
+        z = float(input())
+        if y == 0 or z == 87:
+            print('Incorrect value')
+        else:
+            break
+
+except ValueError:
+    print('Incorrect input')
 
 print(x + (11 - x / y) / (87 - z))
-
