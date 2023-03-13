f2d407830489108b0530159a69fcc5ebd68d8499
Soviet.Dog <soviet.dog1945@proton.me>
2023-03-13 09:11:45 +0200

    done task 3

---
+++ Task 3.py
@@ -0,0 +1,18 @@
+# Multiparadigm programming languages, Task 3
+# Vanda Nikita Denisovich
+import math
+
+try:
+
+    while True:
+        x = float(input())
+        a = float(input())
+        if x == 1 or a == -2:
+            print('Incorrect value')
+        else:
+            break
+except ValueError:
+    print('Incorrect input')
+
+    y = (x ** 3 + 2 * a * x + 3) / ((x - 1) ** 2) + (math.cos(x ** 2)) / (a + 2)
+    print(y)
