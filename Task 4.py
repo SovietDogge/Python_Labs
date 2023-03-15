8d2e9b699683c881a5bfa52bfdadc349f4687614
Soviet.Dog <soviet.dog1945@proton.me>
2023-03-15 18:29:49 +0200

    done Task 4

---
+++ Task 4.py
@@ -0,0 +1,10 @@
+# Multiparadigm programming languages, Task 4
+# Vanda Nikita Denisovich
+
+n = int(input('Enter a num:'))
+
+k = 1
+while k <= n:
+    if k % 10 == 3:
+        print(k)
+    k += 1
