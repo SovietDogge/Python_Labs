ec98dc59a51beb4d7368bf838345f59183bed621
Soviet.Dog <soviet.dog1945@proton.me>
2023-04-16 15:48:03 +0300

    start task1

---
+++ lecture_tasks/task_2.py
@@ -0,0 +1,12 @@
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+FILE = 'company_sales_data.csv'
+
+def task_1():
+    with
+
+if __name__ == '__main__':
+    pass
