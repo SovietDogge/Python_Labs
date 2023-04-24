6ce7c92025b6140a3bebcc397f90bfefd444c078
Soviet.Dog <soviet.dog1945@proton.me>
2023-04-25 00:57:58 +0300

    main task is done

--- task_8_9.py
+++ task_8_9.py
@@ -1,3 +1,7 @@
+import random
+
+from utils import *
+
 EMPTY_USERS_WARNING = 'We need to add some users!!!'
 
 
@@ -90,22 +94,119 @@ def task_5(users_month):
             print(f'{users_month} has 28 in an ordinary year')
 
 
-def task_7_num_validation(num):
-    try:
-        return int(num)
-    except ValueError:
-        return 0
-
-
 def task_7():
     summary = 0
     while True:
-        num = task_7_num_validation(input())
+        num = nums_validation(input())
         if num == 0:
             break
         summary += num
     print(summary)
 
 
+def make_calculations(number1, number2, oper):
+    if oper == '+':
+        return number1 + number2
+    elif oper == '-':
+        return number1 - number2
+    elif oper == '/':
+        try:
+            return number1 / number2
+        except ZeroDivisionError:
+            raise Exception('You can\'t divide by zero')
+    elif oper == '*':
+        return number1 * number2
+    elif oper == 'mod':
+        return number1 % number2
+    elif oper == 'div':
+        try:
+            return number1 // number2
+        except ZeroDivisionError:
+            raise Exception('You can\'t divide by zero')
+    elif oper == 'pow':
+        return number1 ** number2
+
+
+def task_8():
+    num1 = nums_validation(input('Enter first num '))
+    num2 = nums_validation(input('Enter second num '))
+    operation = operation_validation(input('Enter operation '))
+    print(f'Result = {make_calculations(num1, num2, operation)}')
+
+
+def find_banknote_info(value):
+    possible_banknotes = {1: 'Vladimir Velikiy', 2: 'Yroslav Mudryi', 5: 'Bogda Hmelnitzkyi',
+                          10: 'Ivan Mazepa', 20: 'Ivan Franko', 50: 'Mihail Grushevskyi',
+                          100: 'Taras Shevchenko', 200: 'Lesy Ukrainka', 500: 'Grigoriy Skovoroda',
+                          1000: 'Vladimir Vernadskyi'}
+    return possible_banknotes[value], value
+
+
+def task_9():
+    banknote_value = banknote_validation(int(input('Enter a banknote value ')))
+    result = find_banknote_info(banknote_value)
+    print(f'Value {result[1]} is {result[0]}')
+
+
+def define_color(horizon_cor, vertical_cor):
+    if horizon_cor in 'aceg' and vertical_cor % 2 == 1:
+        return 'black'
+    elif horizon_cor in 'bdfh' and vertical_cor % 2 == 0:
+        return 'black'
+    else:
+        return 'white'
+
+
+def task_10():
+    hor_coord = hor_coord_validation(input('Enter horizontal coordinate '))
+    vert_coord = vert_coord_validation(input('Enter vertical coordinate '))
+    result = define_color(hor_coord, vert_coord)
+    print(f'field is {result}')
+
+
+def convert_to_binary(number):
+    result = ''
+    while number > 0:
+        r = number % 2
+        result = str(r) + result
+        number = int(number / 2)
+    return result
+
+
+def convert_to_decimal(number):
+    degree = len(str(number)) - 1
+    new_num = 0
+    i = 0
+    while degree >= 0:
+        if number[i] == '1':
+            new_num += 2 ** degree
+        degree -= 1
+        i += 1
+
+    return new_num
+
+
+def task_11():
+    num = nums_validation(input('Enter any num '))
+    binary_num = convert_to_binary(num)
+    decimal_num = convert_to_decimal(binary_num)
+    print(f'Number {num} in binary = {binary_num}, in Decimal = {decimal_num}')
+
+
+def task_12():
+    user_choice = game_input_validation(input('Enter rock, paper or scissors '))
+    possible_variants = ['rock', 'paper', 'scissors']
+    bot_choice = random.sample(possible_variants, 1)[0]
+    victory_condition = {'rock': 'scissors', 'scissors': 'paper', 'paper': 'rock'}
+    print(f'Bot choose {bot_choice}')
+
+    if user_choice == bot_choice:
+        return 'draw'
+    elif victory_condition[user_choice] == bot_choice:
+        return 'Victory'
+    return 'Defeat'
+
+
 if __name__ == '__main__':
-    task_7()
+    while True:
+        print(task_12())
---
+++ utils.py
@@ -0,0 +1,58 @@
+INCORRECT_INPUT_ER = 'Incorrect input'
+ENTER_CORRECT_NUM = 'Please enter num from 0 to 8: '
+
+
+def nums_validation(num):
+    try:
+        return int(num)
+    except ValueError:
+        print(INCORRECT_INPUT_ER)
+        return nums_validation(input())
+
+
+def operation_validation(oper):
+    possible_operations = ['+', '-', '/', '*', 'mod', 'pow', 'div']
+    if oper in possible_operations:
+        return oper
+    else:
+        print('Write correct operation')
+        return operation_validation(input())
+
+
+def banknote_validation(value):
+    possible_values = [1, 2, 5, 10, 20, 50, 100, 200, 500, 1000]
+    if value in possible_values:
+        return value
+    else:
+        print(INCORRECT_INPUT_ER)
+        return banknote_validation(int(input()))
+
+
+def hor_coord_validation(coordinate):
+    possible_coordinates = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+    if coordinate in possible_coordinates:
+        return coordinate
+    else:
+        print(INCORRECT_INPUT_ER)
+        return hor_coord_validation(input('Please enter another letter: '))
+
+
+def vert_coord_validation(coordinate):
+    try:
+        coordinate = int(coordinate)
+        if 0 < coordinate <= 8:
+            return coordinate
+        else:
+            print(INCORRECT_INPUT_ER)
+            return vert_coord_validation(int(input(ENTER_CORRECT_NUM)))
+    except ValueError:
+        return vert_coord_validation(int(input(ENTER_CORRECT_NUM)))
+
+
+def game_input_validation(value):
+    possible_variants = ['rock', 'paper', 'scissors']
+    if value in possible_variants:
+        return value
+    else:
+        print(INCORRECT_INPUT_ER)
+        return game_input_validation(input('Please, enter rock, paper or scissors '))
