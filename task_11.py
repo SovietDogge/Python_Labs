f58a94e28f226b7521eb27368b562c3bc68402d5
Soviet.Dog <soviet.dog1945@proton.me>
2023-04-25 22:28:26 +0300

    start task 11

---
+++ task_11.py
@@ -0,0 +1,14 @@
+import  re
+
+
+RANDOM_TEXT = 'dates.txt'
+
+def write_correct_dates(file):
+    current_date = '25.04.2023'
+    with open(file, 'r') as text:
+        text =
+
+
+
+if __name__ == '__main__':
+    write_correct_dates(RANDOM_TEXT)
