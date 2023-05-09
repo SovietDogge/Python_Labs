cd01116ec584f42823c7039be7c97c83dd0b640f
Soviet.Dog <soviet.dog1945@proton.me>
2023-05-09 21:11:43 +0300

    start task 2

--- task_11.py
+++ task_11.py
@@ -12,9 +12,24 @@ def write_correct_dates(path):
 
 def task_1(path):
     with open(path, 'rt', encoding='utf-8') as file:
-        dates = re.findall(r'(?:\d{1,2})\.(?:\d{1,2})\.\d{4}', file.read())
-        dates += re.findall(r'(\d{4})[\/](?:\d{1,2})[\/](?:\d{1,2})', file.read())
-        print(dates)
+        text = file.read()
+        dates = re.findall(r'(?:\d{1,2})\.(?:\d{1,2})\.\d{4}', text)
+        dates += re.findall(r'\d{4}[/](?:\d{1,2})[/](?:\d{1,2})', text)
+
+    for i, date in enumerate(dates):
+        date = date.replace('/', '.')
+
+        correct_date = date.split('.')
+        for j, num in enumerate(correct_date):
+            if len(num) < 2:
+                correct_date[j] = f'0{correct_date[j]}'
+
+        if len(correct_date[0]) > len(correct_date[-1]):
+            correct_date[0], correct_date[-1] = correct_date[-1], correct_date[0]
+
+        dates[i] = '.'.join(correct_date)
+
+    print(dates)
 
 
 if __name__ == '__main__':
