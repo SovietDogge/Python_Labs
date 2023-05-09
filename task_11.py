748073695972d41619aa55d38f34e1070fdc2fa9
Soviet.Dog <soviet.dog1945@proton.me>
2023-05-09 18:21:22 +0300

    start from the beginning

--- task_11.py
+++ task_11.py
@@ -4,12 +4,21 @@ import re
 def write_correct_dates(path):
     current_date = '25.04.2023'
     with open(path, 'rt', encoding='utf-8') as file:
-        text = re.sub('\d{2}\.\d{2}\.\d{4}',current_date, file.read())
+        text = re.sub(r'\d{2}\.\d{2}\.\d{4}', current_date, file.read())
 
     with open('updated.txt', 'wt') as file:
         file.write(text)
 
 
+def task_1(path):
+    with open(path, 'rt', encoding='utf-8') as file:
+        dates = re.findall(r'(?:\d{1,2})\.(?:\d{1,2})\.\d{4}', file.read())
+        dates += re.findall(r'(\d{4})[\/](?:\d{1,2})[\/](?:\d{1,2})', file.read())
+        print(dates)
+
+
 if __name__ == '__main__':
-    file_path = 'dates.txt'
-    write_correct_dates(file_path)
+    task_1('dates.txt')
+    # dates_file_path = 'dates.txt'
+    # write_correct_dates(dates_file_path)
+
